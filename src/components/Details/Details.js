ccfd87b7ebeddc87f01344d7152133a9c19ff311
Ibrahim Khalil <ibrahimkhalilik748@gmail.com>
2021-03-13 01:15:15 +0600

    push

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Details.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faFacebook, faYouTube, faTwitter } from '@fortawesome/free-solid-svg-icons'
 
 const Details = (props) => {
-    const {strGender, strLeague, strCountry, strTeamFanart3, strDescriptionEN, strDescriptionDE, strStadium}  = props.teams;
+    const {strGender, strLeague, strCountry, strTeamFanart3, strDescriptionEN, strDescriptionDE, strStadium, strFacebook, strTwitter, strYoutube}  = props.teams;
     return (
         <div className="detailsBody"><br/><br/>
             <div className="banner">
@@ -19,6 +21,11 @@ const Details = (props) => {
             </div><br/>
             <p className="color">{strDescriptionDE}</p><br/>
             <p className="color">{strDescriptionEN}</p><br/>
+            <div className="icon">
+                <a href={strTwitter} target="_blank"><FontAwesomeIcon icon={faCoffee} /></a>
+                <a href={strFacebook} target="_blank"><FontAwesomeIcon icon={faCoffee} /></a>
+                <a href={strYoutube} target="_blank"><FontAwesomeIcon icon={faCoffee} /></a>
+            </div><br/><br/><br/>
         </div>
     );
 };
